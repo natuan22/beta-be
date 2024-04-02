b85e78c453b2fd50075fd1d3d99beb013fac7f40
Chau Minh <minhnlc@aloapp.vn>
2024-04-02 15:27:14 +0700

    fix: token

--- src/guards/admin.guard.ts
+++ src/guards/admin.guard.ts
@@ -1,13 +1,16 @@
-import {CanActivate, ExecutionContext, HttpStatus, Injectable,} from '@nestjs/common';
-import {ExceptionResponse} from '../exceptions/common.exception';
-import {MRequest} from '../types/middleware';
-import {JwtService} from "@nestjs/jwt";
-import {RoleEnum} from "../enums/auth.enum";
+import { CanActivate, ExecutionContext, HttpStatus, Injectable, } from '@nestjs/common';
+import { ExceptionResponse } from '../exceptions/common.exception';
+import { MRequest } from '../types/middleware';
+import { JwtService } from "@nestjs/jwt";
+import { RoleEnum } from "../enums/auth.enum";
+import { AuthService } from '../auth/auth.service';
+import { DeviceEntity } from '../auth/entities/device.entity';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
   constructor(
-      private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
+    private readonly authService: AuthService,
   ) {
   }
 
@@ -26,6 +29,20 @@ export class AdminGuard implements CanActivate {
       throw new ExceptionResponse(HttpStatus.FORBIDDEN, 'You are not allowed to do that!')
     }
 
-    return true;
+    // Lấy deviceId từ token và từ request
+    const deviceId: string = this.jwtService.decode(token)?.['deviceId'];
+    
+    // Lấy secret key từ authService và kiểm tra tính hợp lệ của token
+    const { secret_key, expired_at }: Awaited<Pick<DeviceEntity, "secret_key" | "expired_at">>
+      = await this.authService.getSecretKey(deviceId);
+
+    // Kiểm tra thiết bị còn hợp lệ hay không
+    if (expired_at < new Date()) {
+      throw new ExceptionResponse(HttpStatus.UNAUTHORIZED, 'device is expired');
+    }
+
+    // Kiểm tra tính hợp lệ của token
+    return !!this.jwtService.verify(token, { secret: secret_key });
+
   }
 }
