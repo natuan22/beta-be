3f3dffda2ecc86f62cc8c003034753a3fe087380
Chau Minh <minhnlc@aloapp.vn>
2024-04-11 15:25:35 +0700

    .

--- src/shares/dto/stock.dto.ts
+++ src/shares/dto/stock.dto.ts
@@ -4,7 +4,7 @@ import { IsNotEmpty, IsString, Length } from "class-validator";
 export class StockDto {
     @IsNotEmpty({message: 'stock not found'})
     @IsString()
-    @Length(3, 5, {message: 'stock chỉ đc từ 3 đến 5 kí tự'})
+    @Length(1, 5, {message: 'stock chỉ đc từ 1 đến 5 kí tự'})
     @ApiProperty({
         type: String
     })
