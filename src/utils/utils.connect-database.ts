56c55f6c3432317822e0aec5435eaaf077a13fc9
Chau Minh <minhnlc@aloapp.vn>
2024-05-20 08:55:05 +0700

    fix: connect db

--- src/utils/utils.connect-database.ts
+++ src/utils/utils.connect-database.ts
@@ -1,7 +1,7 @@
 const sql = require('mssql')
 
 const connectDB = async () => {
-   return await sql.connect(`Server=${process.env.DB_HOST},1433;Database=PHANTICH;User Id=THANHMINH;Password=123beta456;Encrypt=false;Request Timeout=30000`);
+   return await sql.connect(`Server=${process.env.DB_HOST},1433;Database=PHANTICH;User Id=${process.env.DB_USERNAME};Password=${process.env.DB_PASSWORD};Encrypt=false;Request Timeout=30000`);
 }
 
 export default connectDB;
