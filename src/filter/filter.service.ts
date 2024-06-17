6a87e6b3b0fc8893a8323521f8ea66442fe054c8
Chau Minh <minhnlc@aloapp.vn>
2024-06-17 14:57:01 +0700

    .

--- src/filter/filter.service.ts
+++ src/filter/filter.service.ts
@@ -20,58 +20,69 @@ export class FilterService {
 
       const day_52_week = moment().subtract(52, 'week').format('YYYY-MM-DD')
       const query = `
-      with temp as (select code, floor,
-              TyleNDTNNdangnamgiu,
-              closePrice, 
-              lead(closePrice) over (partition by code order by date desc) as closePrice_pre,
-              min_1_week, max_1_week, min_1_month, max_1_month, min_3_month, max_3_month, min_6_month, max_6_month, min_1_year, max_1_year,
-              perChange1D, perChange5D, perChange1M, perChange3M, perChange6M, perChange1Y, perChangeYTD, beta,
-              volume as totalVol, TyLeKLMBCD, KNnetVal, KNnetVol,
-              ma5, ma10, ma20, ma50, ma100, ma200, ema5, ema10, ema20, ema50, ema100, ema200,
-              lead(ma5) over (partition by code order by date desc) as ma5_pre,
-              lead(ma10) over (partition by code order by date desc) as ma10_pre,
-              lead(ma20) over (partition by code order by date desc) as ma20_pre,
-              lead(ma50) over (partition by code order by date desc) as ma50_pre,
-              lead(ma100) over (partition by code order by date desc) as ma100_pre,
-              lead(ma200) over (partition by code order by date desc) as ma200_pre,
-              lead(ema5) over (partition by code order by date desc) as ema5_pre,
-              lead(ema10) over (partition by code order by date desc) as ema10_pre,
-              lead(ema20) over (partition by code order by date desc) as ema20_pre,
-              lead(ema50) over (partition by code order by date desc) as ema50_pre,
-              lead(ema100) over (partition by code order by date desc) as ema100_pre,
-              lead(ema200) over (partition by code order by date desc) as ema200_pre,
-              rsi,
-              lead(rsi) over (partition by code order by date desc) as rsi_pre,
+      WITH latest_dates AS (
+          SELECT DISTINCT TOP 2 date
+          FROM VISUALIZED_DATA.dbo.filterResource
+          ORDER BY date DESC
+      ),
+      temp AS (
+          SELECT 
+              code, floor, TyleNDTNNdangnamgiu, closePrice,
+              LEAD(closePrice) OVER (PARTITION BY code ORDER BY date DESC) AS closePrice_pre,
+              min_1_week, max_1_week, min_1_month, max_1_month, min_3_month, max_3_month, 
+              min_6_month, max_6_month, min_1_year, max_1_year,
+              perChange1D, perChange5D, perChange1M, perChange3M, perChange6M, 
+              perChange1Y, perChangeYTD, beta, volume AS totalVol, TyLeKLMBCD, 
+              KNnetVal, KNnetVol,
+              ma5, ma10, ma20, ma50, ma100, ma200, 
+              ema5, ema10, ema20, ema50, ema100, ema200,
+              LEAD(ma5) OVER (PARTITION BY code ORDER BY date DESC) AS ma5_pre,
+              LEAD(ma10) OVER (PARTITION BY code ORDER BY date DESC) AS ma10_pre,
+              LEAD(ma20) OVER (PARTITION BY code ORDER BY date DESC) AS ma20_pre,
+              LEAD(ma50) OVER (PARTITION BY code ORDER BY date DESC) AS ma50_pre,
+              LEAD(ma100) OVER (PARTITION BY code ORDER BY date DESC) AS ma100_pre,
+              LEAD(ma200) OVER (PARTITION BY code ORDER BY date DESC) AS ma200_pre,
+              LEAD(ema5) OVER (PARTITION BY code ORDER BY date DESC) AS ema5_pre,
+              LEAD(ema10) OVER (PARTITION BY code ORDER BY date DESC) AS ema10_pre,
+              LEAD(ema20) OVER (PARTITION BY code ORDER BY date DESC) AS ema20_pre,
+              LEAD(ema50) OVER (PARTITION BY code ORDER BY date DESC) AS ema50_pre,
+              LEAD(ema100) OVER (PARTITION BY code ORDER BY date DESC) AS ema100_pre,
+              LEAD(ema200) OVER (PARTITION BY code ORDER BY date DESC) AS ema200_pre,
+              rsi, LEAD(rsi) OVER (PARTITION BY code ORDER BY date DESC) AS rsi_pre,
               macd, macd_signal,
-              lead(macd) over (partition by code order by date desc) as macd_pre,
-              lead(macd_signal) over (partition by code order by date desc) as macd_signal_pre,
-              BBL,
-              BBU,
-              doanh_thu_4_quy, loi_nhuan_4_quy,
+              LEAD(macd) OVER (PARTITION BY code ORDER BY date DESC) AS macd_pre,
+              LEAD(macd_signal) OVER (PARTITION BY code ORDER BY date DESC) AS macd_signal_pre,
+              BBL, BBU, doanh_thu_4_quy, loi_nhuan_4_quy,
               tang_truong_doanh_thu_4_quy, tang_truong_loi_nhuan_4_quy,
-              qoq_doanh_thu, qoq_loi_nhuan,
-              yoy_doanh_thu, yoy_loi_nhuan,
+              qoq_doanh_thu, qoq_loi_nhuan, yoy_doanh_thu, yoy_loi_nhuan,
               EPS, BVPS, PE, PB, PS, marketCap,
-              TinHieuChiBaoKyThuat as tech,
-              TinHieuDuongXuHuong as trend,
-              TinHieuTongHop as overview,
-              date
-      from VISUALIZED_DATA.dbo.filterResource where date in (select distinct top 2 date from VISUALIZED_DATA.dbo.filterResource order by date desc)),
-      min_max as (
-        select code, min(closePrice) as PRICE_LOWEST_CR_52W, max(closePrice) as PRICE_HIGHEST_CR_52W from marketTrade.dbo.tickerTradeVND where date >= '${day_52_week}' group by code
+              TinHieuChiBaoKyThuat AS tech, TinHieuDuongXuHuong AS trend, 
+              TinHieuTongHop AS overview, date
+          FROM VISUALIZED_DATA.dbo.filterResource
+          WHERE date IN (SELECT date FROM latest_dates)
+      ),
+      min_max AS (
+          SELECT 
+              code, 
+              MIN(closePrice) AS PRICE_LOWEST_CR_52W, 
+              MAX(closePrice) AS PRICE_HIGHEST_CR_52W 
+          FROM marketTrade.dbo.tickerTradeVND 
+          WHERE date >= '${day_52_week}'
+          GROUP BY code
       )
-      select t.*, m.totalVal, i.LV4,
-      r.netVal as buyVal, r.netVol as buyVol,
-      b.MB, b.Mua, b.Ban,  
-      l.PRICE_HIGHEST_CR_52W, l.PRICE_LOWEST_CR_52W
-      from temp t
-      inner join marketInfor.dbo.info i on i.code = t.code
-      inner join marketTrade.dbo.tickerTradeVND m on m.code = t.code and m.date = t.date
-      inner join PHANTICH.dbo.MBChuDong b on b.MCK = t.code and b.Ngay = (select max(Ngay) from PHANTICH.dbo.MBChuDong)
-      inner join marketTrade.dbo.[foreign] r on r.code = t.code and r.date = (select max(date) from marketTrade.dbo.[foreign] where type = 'STOCK')
-      left join min_max l on l.code = t.code
-      where t.date = (select max(date) from temp)
-      and i.status = 'listed'
+      SELECT 
+          t.*, m.totalVal, i.LV4,
+          r.netVal AS buyVal, r.netVol AS buyVol,
+          b.MB, b.Mua, b.Ban,
+          l.PRICE_HIGHEST_CR_52W, l.PRICE_LOWEST_CR_52W
+      FROM temp t
+      INNER JOIN marketInfor.dbo.info i ON i.code = t.code
+      INNER JOIN marketTrade.dbo.tickerTradeVND m ON m.code = t.code AND m.date = t.date
+      INNER JOIN PHANTICH.dbo.MBChuDong b ON b.MCK = t.code AND b.Ngay = (SELECT MAX(Ngay) FROM PHANTICH.dbo.MBChuDong)
+      INNER JOIN marketTrade.dbo.[foreign] r ON r.code = t.code AND r.date = (SELECT MAX(date) FROM marketTrade.dbo.[foreign] WHERE type = 'STOCK')
+      LEFT JOIN min_max l ON l.code = t.code
+      WHERE t.date = (SELECT MAX(date) FROM temp)
+      AND i.status = 'listed';
       `
 
       //Query lấy totalVol trung bình
@@ -138,32 +149,33 @@ export class FilterService {
 
       //roe, roa
       const query_4 = `
-      with roae as (
-        select code,
-               ROE as roae,
-               ROA as roaa,
-               yearQuarter
-      from RATIO.dbo.ratioInYearQuarter
-      where right(yearQuarter, 1) <> 0
+      WITH roae AS (
+          SELECT code,
+                ROE AS roae,
+                ROA AS roaa,
+                yearQuarter
+          FROM RATIO.dbo.ratioInYearQuarter
+          WHERE RIGHT(yearQuarter, 1) <> '0'
+          AND yearQuarter >= '20224'
       ),
-      sum_roaa as (
-        select roaa +
-        lead(roaa) over (partition by code order by yearQuarter desc)
-        + lead(roaa, 2) over (partition by code order by yearQuarter desc)
-         + lead(roaa, 3) over (partition by code order by yearQuarter desc)
-        as roaa,
-          roae +
-              lead(roae) over (partition by code order by yearQuarter desc)
-              + lead(roae, 2) over (partition by code order by yearQuarter desc)
-              + lead(roae, 3) over (partition by code order by yearQuarter desc)
-        as roae, code, yearQuarter
-        from roae
+      sum_roaa AS (
+          SELECT
+              code,
+              yearQuarter,
+              roaa + LEAD(roaa, 1) OVER (PARTITION BY code ORDER BY yearQuarter DESC) +
+                    LEAD(roaa, 2) OVER (PARTITION BY code ORDER BY yearQuarter DESC) +
+                    LEAD(roaa, 3) OVER (PARTITION BY code ORDER BY yearQuarter DESC) AS roaa,
+              roae + LEAD(roae, 1) OVER (PARTITION BY code ORDER BY yearQuarter DESC) +
+                    LEAD(roae, 2) OVER (PARTITION BY code ORDER BY yearQuarter DESC) +
+                    LEAD(roae, 3) OVER (PARTITION BY code ORDER BY yearQuarter DESC) AS roae
+          FROM roae
       ),
-        roaa
-      AS (
-        select code, roaa as ROA, roae as ROE from sum_roaa where yearQuarter = (select max(yearQuarter) from sum_roaa)
+      max_yearQuarter AS (
+          SELECT MAX(yearQuarter) AS maxYQ FROM sum_roaa
       )
-        select * from roaa
+      SELECT s.code, s.roaa AS ROA, s.roae AS ROE
+      FROM sum_roaa s
+      JOIN max_yearQuarter m ON s.yearQuarter = m.maxYQ;
       `;
 
       const [data, data_2, data_3, data_4] = await Promise.all(
@@ -174,7 +186,7 @@ export class FilterService {
         this.mssqlService.query(query_4)
         ])
       const dataMapped = FilterResponse.mapToList(data, data_2, data_3, data_4)
-      await this.redis.set('filter2', dataMapped, { ttl: 60 })
+      await this.redis.set('filter2', dataMapped, { ttl: 5000 })
       return dataMapped
     } catch (e) {
       throw new CatchException(e)
